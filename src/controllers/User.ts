c0f501ee5ea09897c88c4508365f90d0d66a2f80
Jonathan Ferreira <j0n4t@hotmail.com>
2022-02-25 16:12:52 -0300

    fix: ajusta códigos e adiciona validações

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -4,12 +4,12 @@ import userService from '../services/User';
 const create = async (req: Request, res: Response) => {
   const { username, classe, level, password } = req.body;
   const user = await userService.create({ username, classe, level, password });
-  res.send({ token: user });
+  res.status(201).send({ token: user }).end();
 };
 
 const getAll = async (_req: Request, res: Response) => {
   const users = await userService.getAll();
-  res.send(users);
+  res.status(200).send(users).end();
 };
 
 export default {
--- src/routers/User.ts
+++ src/routers/User.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import userController from '../controllers/User';
+import validateNewUser from '../middlewares/User';
 
 const userRouter = express.Router();
 
-userRouter.post('/', userController.create);
+userRouter.post('/', validateNewUser, userController.create);
 userRouter.get('/', userController.getAll);
 
 export default userRouter;
