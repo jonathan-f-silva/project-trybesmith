a67199634539acf199c873e11a6ce96d6e22e9c6
Jonathan Ferreira <j0n4t@hotmail.com>
2022-02-25 16:11:23 -0300

    chore: adiciona mensagens de erro e códigos

---
+++ src/utils/ErrorMessages.ts
@@ -0,0 +1,44 @@
+import HTTPCodes from './HTTPCodes';
+
+type TErrorMessagesTypes = {
+  [key: string]: string;
+};
+
+type TErrorMessagesCodes = {
+  [key: string]: number;
+};
+
+const ErrorMessageTypes: TErrorMessagesTypes = {
+  USER_REQUIRED: 'Username is required',
+  USER_STRING: 'Username must be a string',
+  USER_MIN_LENGTH: 'Username must be longer than 2 characters',
+  CLASS_REQUIRED: 'Classe is required',
+  CLASS_STRING: 'Classe must be a string',
+  CLASS_MIN_LENGTH: 'Classe must be longer than 2 characters',
+  LEVEL_REQUIRED: 'Level is required',
+  LEVEL_NUMBER: 'Level must be a number',
+  LEVEL_GREATER_THAN_ZERO: 'Level must be greater than 0',
+  PASSWORD_REQUIRED: 'Password is required',
+  PASSWORD_STRING: 'Password must be a string',
+  PASSWORD_MIN_LENGTH: 'Password must be longer than 7 characters',
+};
+
+const ErrorCodeByMessage: TErrorMessagesCodes = {
+  [ErrorMessageTypes.USER_STRING]: HTTPCodes.UNPROCESSABLE_ENTITY,
+  [ErrorMessageTypes.USER_REQUIRED]: HTTPCodes.BAD_REQUEST,
+  [ErrorMessageTypes.USER_MIN_LENGTH]: HTTPCodes.UNPROCESSABLE_ENTITY,
+  [ErrorMessageTypes.CLASS_REQUIRED]: HTTPCodes.BAD_REQUEST,
+  [ErrorMessageTypes.CLASS_STRING]: HTTPCodes.UNPROCESSABLE_ENTITY,
+  [ErrorMessageTypes.CLASS_MIN_LENGTH]: HTTPCodes.UNPROCESSABLE_ENTITY,
+  [ErrorMessageTypes.LEVEL_REQUIRED]: HTTPCodes.BAD_REQUEST,
+  [ErrorMessageTypes.LEVEL_NUMBER]: HTTPCodes.UNPROCESSABLE_ENTITY,
+  [ErrorMessageTypes.LEVEL_GREATER_THAN_ZERO]: HTTPCodes.UNPROCESSABLE_ENTITY,
+  [ErrorMessageTypes.PASSWORD_REQUIRED]: HTTPCodes.BAD_REQUEST,
+  [ErrorMessageTypes.PASSWORD_STRING]: HTTPCodes.UNPROCESSABLE_ENTITY,
+  [ErrorMessageTypes.PASSWORD_MIN_LENGTH]: HTTPCodes.UNPROCESSABLE_ENTITY,
+};
+
+export { 
+  ErrorCodeByMessage,
+  ErrorMessageTypes,
+};
